97eeb01a2bd9ff129938912e0a855c34291b76db
Charlotte Li <charlotte_ljh@163.com>
2019-07-21 22:58:55 +0800

    init

---
+++ src/test/java/com/charlotte/rabbitmqlearning/RabbitmqLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.charlotte.rabbitmqlearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqLearningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
